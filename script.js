f672a307f2a9b0811453993886e26e03cac78fa3
drony0610 <51791640+drony0610@users.noreply.github.com>
2020-03-27 13:14:09 +0300

    Services and advantages

---
+++ script.js
@@ -0,0 +1,13 @@
+window.onload = function(){
+
+var cards_array = document.querySelectorAll(".card-item");
+for(var i = 0; i<cards_array.length; i++){
+    cards_array[i].addEventListener("mouseover",function(){
+        if(i == 0){
+            document.querySelector(".img-design").style
+        }
+    });
+};
+
+
+};
