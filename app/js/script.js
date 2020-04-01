49189b5e3d34e2cb4600e87ced26cab3321545d0
drony0610 <51791640+drony0610@users.noreply.github.com>
2020-04-01 13:59:36 +0300

    v1.0

---
+++ app/js/script.js
@@ -0,0 +1,17 @@
+window.onload = function(){
+alert("connected");
+var wraped_text = document.querySelectorAll(".advantage-card__text");
+var hovered_title = document.querySelectorAll(".advantage-card__title");
+for(var i = 0; i<hovered_title.length; i++){
+    hovered_title[i].addEventListener("mouseover", function(){
+       
+            
+        }
+    });
+
+
+}
+
+
+
+}
